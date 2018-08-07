9adc211f882db78ca7af7336bbf7cb124a223da5
nishimunea <nishimunea@gmail.com>
2018-08-08 05:58:03 +0900

    Call Chalice's deploy/delete command from Terraform apply/destroy

--- main.tf
+++ main.tf
@@ -6,4 +6,8 @@ resource "null_resource" "chalice" {
   provisioner "local-exec" {
     command = "chalice deploy --stage dev"
   }
+  provisioner "local-exec" {
+    when = "destroy"
+    command = "chalice delete"
+  }
 }
