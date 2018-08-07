15d0bab098fcbc78374719ae40e8e32476741ca1
nishimunea <nishimunea@gmail.com>
2018-08-07 09:44:03 +0900

    Remove aws credentials setting from terraform files

--- main.tf
+++ main.tf
@@ -1,14 +1,5 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {
-  default = "ap-northeast-1"
-}
- 
 module "aws" {
   source = "./terraform"
-  aws_access_key = "${var.aws_access_key}"
-  aws_secret_key = "${var.aws_secret_key}"
-  aws_region = "${var.aws_region}"
 }
 
 resource "null_resource" "chalice" {
--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,13 +1,4 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {
-  default = "ap-northeast-1"
-}
-
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region = "${var.aws_region}"
 }
 
 module "dynamodb" {
