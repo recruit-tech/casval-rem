53ea6ffab1e53911c654a3acc4336bec2d50dbe1
MUNEAKI NISHIMURA <nishimunea@gmail.com>
2018-08-09 07:14:21 +0900

    Enqueue scan request to SQS (#5)
    * Add enque process in PATCH /scan/{id}/schedule
    
    * Remove SQS queue URL retrieving process from Terraterm scripts

--- terraform/sqs/scan_waiting_queue.tf
+++ terraform/sqs/scan_waiting_queue.tf
@@ -2,7 +2,4 @@ resource "aws_sqs_queue" "scan_waiting_queue" {
   name                        = "ScanWaiting"
   visibility_timeout_seconds  = 60
   message_retention_seconds   = 1209600
-  provisioner "local-exec" {
-    command = "echo SCAN_WAITING_QUEUE_URL = \\\"${aws_sqs_queue.scan_waiting_queue.id}\\\" > ./chalicelib/env/scan_waiting_queue.py"
-  }
 }
