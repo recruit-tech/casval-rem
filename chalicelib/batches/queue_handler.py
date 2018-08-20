f1bfea6be7afaf1a7da21b57d447cb80bc2aad60
nishimunea <nishimunea@gmail.com>
2018-08-21 00:31:54 +0900

    Add stopped queue process

--- chalicelib/batches/queue_handler.py
+++ chalicelib/batches/queue_handler.py
@@ -20,12 +20,17 @@ class QueueHandler:
         jst = pytz.timezone("Asia/Tokyo")
         self.now = datetime.now(tz=pytz.utc).astimezone(jst)
 
-    def __disable_if_submitted(func):
-        def disable_if_submitted_wrapper(self, *args, **kwargs):
-            if self.audit["submitted"] is True:
-                raise Exception("audit-submitted")
-            else:
-                return func(self, *args, **kwargs)
+    def process_scan_pending_queue(self):
+        pending_queue = Queue(Queue.SCAN_PENDING)
+        return self.__process_all_messages(pending_queue, self.__launch_scan)
+
+    def process_scan_running_queue(self):
+        running_queue = Queue(Queue.SCAN_RUNNING)
+        return self.__process_all_messages(running_queue, self.__check_progress)
+
+    def process_scan_stopped_queue(self):
+        stopped_queue = Queue(Queue.SCAN_RUNNING)
+        return self.__process_all_messages(stopped_queue, self.__notify_result)
 
     def __exception_handler(func):
         def self_wrapper(self, *args, **kwargs):
@@ -38,170 +43,132 @@ class QueueHandler:
         return self_wrapper
 
     @__exception_handler
-    def process_scan_pending_queue(self):
-        # TODO: Use singleton queue instance for reducing SQS loading time
-        pending_queue = Queue(Queue.SCAN_PENDING)
-        running_queue = Queue(Queue.SCAN_RUNNING)
-        stopped_queue = Queue(Queue.SCAN_STOPPED)
-
+    def __process_all_messages(self, queue, func):
         while 1:
-            messages = pending_queue.peek()
+            messages = queue.peek()
             self.app.log.debug("Messages obtained: " + str(len(messages)))
 
             if len(messages) is 0:
-                break
-
-            for message in messages:
-                entry = {"Id": message.message_id, "ReceiptHandle": message.receipt_handle}
-                body = json.loads(message.body)
-                self.app.log.debug("Message: body=" + message.body)
-
-                start_at = self.__get_datetime_in_utc(body["start_at"])
-                end_at = self.__get_datetime_in_utc(body["end_at"])
-
-                if end_at < (self.now + timedelta(hours=1)):
-                    self.app.log.debug("Scan abandoned: schedule_uuid=" + body["schedule_uuid"])
-                    body["error"] = "Scan has been abandoned because 'end_at' is soon or over."
-                    stopped_queue.enqueue(body)
-                    pending_queue.delete(entry)
-                    continue
-
-                if start_at > self.now:
-                    # The time to scan hasn't come yet, check next message.
-                    continue
-
-                num_of_running_scan = running_queue.message_count()
-                if num_of_running_scan >= int(os.getenv("MAX_PARALLEL_SCAN_SESSION")):
-                    # Abandon subsequent process because other scan sessions are running.
-                    if num_of_running_scan > int(os.getenv("MAX_PARALLEL_SCAN_SESSION")):
-                        self.app.log.error("Running scan: " + str(num_of_running_scan))
-                    self.app.log.debug(
-                        "Polling finished because number of running scan is " + str(num_of_running_scan)
-                    )
-                    return True
-
-                if self.__is_scan_schedule_active(body["schedule_uuid"]) is False:
-                    # Remove the message silently because the scan session has been cancelled by user.
-                    self.app.log.debug(
-                        "Scan removed: scan has been cancelled: schedule_uuid=" + body["schedule_uuid"]
-                    )
-                    pending_queue.delete(entry)
-                    continue
-
-                self.app.log.debug("Scan launched: schedule_uuid=" + body["schedule_uuid"])
-                scanner = Scanner(os.getenv("SCANNER"))
-                session = scanner.launch(body["target"])
-                body["session"] = session
-                running_queue.enqueue(body)
-                pending_queue.delete(entry)
-                # Do not continue subsequent process when new scan has been launched
-                # because SQS sometimes returns wrong `num_of_running_scan` shortly after enqueueing
-                # and that leads the handler to make a mistake on checking of parallel scaning sessions.
                 return True
 
-        return True
+            for message in messages:
+                entry = {"Id": message.message_id, "ReceiptHandle": message.receipt_handle}
+                body = json.loads(message.body)
+                result = func(queue, entry, body)
+                if result is False:
+                    return False
 
-    @__exception_handler
-    def process_scan_running_queue(self):
-        # TODO: Use singleton queue instance for reducing SQS loading time
+    def __launch_scan(self, pending_queue, entry, body):
+        start_at = self.__get_datetime_in_utc(body["start_at"])
+        end_at = self.__get_datetime_in_utc(body["end_at"])
+
+        if end_at < (self.now + timedelta(hours=1)):
+            self.app.log.debug("Scan abandoned: schedule_uuid=" + body["schedule_uuid"])
+            body["error"] = "Scan has been abandoned because 'end_at' is soon or over."
+            stopped_queue = Queue(Queue.SCAN_STOPPED)
+            stopped_queue.enqueue(body)
+            pending_queue.delete(entry)
+            return True
+
+        if start_at > self.now:
+            # The time to scan hasn't come yet, check next message.
+            return True
+
         running_queue = Queue(Queue.SCAN_RUNNING)
-        stopped_queue = Queue(Queue.SCAN_STOPPED)
+        num_of_running_scan = running_queue.message_count()
+        if num_of_running_scan >= int(os.getenv("MAX_PARALLEL_SCAN_SESSION")):
+            # Abandon subsequent process because other scan sessions are running.
+            if num_of_running_scan > int(os.getenv("MAX_PARALLEL_SCAN_SESSION")):
+                self.app.log.error("Running scan: " + str(num_of_running_scan))
+            self.app.log.debug(
+                "Polling finished because number of running scan is " + str(num_of_running_scan)
+            )
+            return False
+
+        if self.__is_scan_schedule_active(body["schedule_uuid"]) is False:
+            # Remove the message silently because the scan session has been cancelled by user.
+            self.app.log.debug(
+                "Scan removed: scan has been cancelled: schedule_uuid=" + body["schedule_uuid"]
+            )
+            pending_queue.delete(entry)
+            return True
+
+        self.app.log.debug("Scan launched: schedule_uuid=" + body["schedule_uuid"])
+        scanner = Scanner(os.getenv("SCANNER"))
+        session = scanner.launch(body["target"])
+        body["session"] = session
+        running_queue.enqueue(body)
+        pending_queue.delete(entry)
+        # Do not continue subsequent process when new scan has been launched
+        # because SQS sometimes returns wrong `num_of_running_scan` shortly after enqueueing
+        # and that leads the handler to make a mistake on checking of parallel scaning sessions.
+        return False
+
+    def __check_progress(self, running_queue, entry, body):
         scanner = Scanner(os.environ["SCANNER"])
+        stopped_queue = Queue(Queue.SCAN_STOPPED)
+        if self.__is_scan_schedule_active(body["schedule_uuid"]) is False:
+            # Force terminate the scan because scan session has been cancelled by user.
+            self.app.log.debug("Scan terminated by user cancellation: schedule_uuid=" + body["schedule_uuid"])
+            scanner.terminate(body["session"])
+            running_queue.delete(entry)
+            return True
 
-        while 1:
-            messages = running_queue.peek()
-            self.app.log.debug("Messages obtained: " + str(len(messages)))
+        end_at = self.__get_datetime_in_utc(body["end_at"])
+        if end_at <= self.now:
+            self.app.log.debug("Scan terminated by timeout: schedule_uuid=" + body["schedule_uuid"])
+            scanner.terminate(body["session"])
+            body["error"] = "Scan has been terminated because 'end_at' is over."
+            stopped_queue.enqueue(body)
+            running_queue.delete(entry)
+            return True
 
-            if len(messages) is 0:
-                break
-
-            for message in messages:
-                entry = {"Id": message.message_id, "ReceiptHandle": message.receipt_handle}
-                body = json.loads(message.body)
-                self.app.log.debug("Message: body=" + message.body)
-
-                if self.__is_scan_schedule_active(body["schedule_uuid"]) is False:
-                    # Force terminate the scan because scan session has been cancelled by user.
-                    self.app.log.debug(
-                        "Scan terminated by user cancellation: schedule_uuid=" + body["schedule_uuid"]
-                    )
-                    scanner.terminate(body["session"])
-                    running_queue.delete(entry)
-                    continue
-
-                end_at = self.__get_datetime_in_utc(body["end_at"])
-                if end_at <= self.now:
-                    self.app.log.debug("Scan terminated by timeout: schedule_uuid=" + body["schedule_uuid"])
-                    scanner.terminate(body["session"])
-                    body["error"] = "Scan has been terminated because 'end_at' is over."
-                    stopped_queue.enqueue(body)
-                    running_queue.delete(entry)
-                    continue
-
-                status = scanner.check_status(body["session"])
-                self.app.log.info("Scan status: {status}".format(status=status))
-                if status == "complete":
-                    stopped_queue.enqueue(body)
-                    running_queue.delete(entry)
-                elif status == "failure":
-                    body["error"] = "Scan session has been failed."
-                    stopped_queue.enqueue(body)
-                    running_queue.delete(entry)
-                else:
-                    # Do not remove the queue entry because scan session is still running.
-                    self.app.log.info("Running: {schedule_uuid}".format(schedule_uuid=body["schedule_uuid"]))
-
+        status = scanner.check_status(body["session"])
+        self.app.log.info("Scan status: {status}".format(status=status))
+        if status == "complete":
+            stopped_queue.enqueue(body)
+            running_queue.delete(entry)
+        elif status == "failure":
+            body["error"] = "Scan session has been failed."
+            stopped_queue.enqueue(body)
+            running_queue.delete(entry)
+        else:
+            # Do not remove the queue entry because scan session is still running.
+            self.app.log.info("Running: {schedule_uuid}".format(schedule_uuid=body["schedule_uuid"]))
         return True
 
-    @__exception_handler
-    def process_scan_stopped_queue(self):
-        stopped_queue = Queue(Queue.SCAN_STOPPED)
+    def __notify_result(self, stopped_queue, entry, body):
         scanner = Scanner(os.environ["SCANNER"])
+        if "error" in body:
+            scan = {}
+            scan["error_reason"] = body["error"]
+            scan["schedule_uuid"] = None
+            scan["scheduled"] = False
+            scan["processed"] = True
+            query = Scan.update(scan).where(Scan.schedule_uuid == body["schedule_uuid"])
+            query.execute()
+            stopped_queue.delete(entry)
+        else:
+            report = self.__load_report(body["scan_uuid"])
+            if report is None:
+                self.app.log.debug("Report not found. Retrieve from scanner")
+                report = scanner.get_report(body["session"])
+                self.app.log.debug("Report retrieved: {} bytes".format(len(report)))
+                result = self.__store_report(body["scan_uuid"], report)
 
-        while 1:
-            messages = stopped_queue.peek()
-            self.app.log.debug("Messages obtained: " + str(len(messages)))
+            self.app.log.debug("Parse report")
+            result = scanner.parse_report(report)
 
-            if len(messages) is 0:
-                break
-
-            for message in messages:
-                entry = {"Id": message.message_id, "ReceiptHandle": message.receipt_handle}
-                body = json.loads(message.body)
-                self.app.log.debug("Message: body=" + message.body)
-
-                if "error" in body:
-                    scan = {}
-                    scan["error_reason"] = body["error"]
-                    scan["schedule_uuid"] = None
-                    scan["scheduled"] = False
-                    scan["processed"] = True
-                    query = Scan.update(scan).where(Scan.schedule_uuid == body["schedule_uuid"])
-                    query.execute()
-                    stopped_queue.delete(entry)
-                else:
-                    report = self.__load_report(body["scan_uuid"])
-                    if report is None:
-                        self.app.log.debug("Report not found. Retrieve from scanner")
-                        report = scanner.get_report(body["session"])
-                        self.app.log.debug("Report retrieved: {} bytes".format(len(report)))
-                        result = self.__store_report(body["scan_uuid"], report)
-
-                    self.app.log.debug("Parse report")
-                    result = scanner.parse_report(report)
-
-                    with db.atomic():
-                        scan = {}
-                        scan["error_reason"] = ""
-                        scan["schedule_uuid"] = None
-                        scan["scheduled"] = False
-                        scan["processed"] = True
-                        query = Scan.update(scan).where(Scan.schedule_uuid == body["schedule_uuid"])
-                        query.execute()
-                        # TODO: Update result table
-                    stopped_queue.delete(entry)
-
-        return True
+            with db.atomic():
+                scan = {}
+                scan["error_reason"] = ""
+                scan["schedule_uuid"] = None
+                scan["scheduled"] = False
+                scan["processed"] = True
+                query = Scan.update(scan).where(Scan.schedule_uuid == body["schedule_uuid"])
+                query.execute()
+                # TODO: Update result table
+            stopped_queue.delete(entry)
 
     def __load_report(self, scan_uuid):
         try:
