afd0df7c6389f012f7d3fb877454651ee2dd54f9
nishimunea <nishimunea@gmail.com>
2019-04-17 08:25:47 +0900

    Add Swagger UI settings

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ from apis import api
 app = Flask(__name__)
 
 app.config["RESTPLUS_MASK_SWAGGER"] = False
+app.config["SWAGGER_UI_REQUEST_DURATION"] = True
+app.config["SWAGGER_UI_DOC_EXPANSION"] = "list"
+app.config["SWAGGER_UI_JSONEDITOR"] = True
 
 api.init_app(app)
